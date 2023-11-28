47fe163e1128fde97f263bf7834c8791b068db8f
Nick Fenwick <neek@nickfenwick.com>
2023-11-28 15:35:30 +0700

    Version v1.0.1.
    Rename 1.0.0 to src, releases are trivial at this point.

--- make-dist.sh
+++ make-dist.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-VERSION=1.0.0
-DIST=soundexsearch-${VERSION}.zip
+VERSION=1.0.1
+SRC=src
+DIST=soundexsearch-v${VERSION}.zip
 if [ -f "$DIST" ] ; then
 	rm "$DIST"
 fi
 
-zip -r "$DIST" "${VERSION}" README.md gpl-3.0.md resources
+zip -r "$DIST" "${SRC}" README.md gpl-3.0.md resources
 echo Made $DIST
--- src/YOUR_ADMIN/includes/extra_datafiles/soundex_search_datafiles.php
+++ src/YOUR_ADMIN/includes/extra_datafiles/soundex_search_datafiles.php
@@ -4,8 +4,8 @@
  * Copyright (C) 2023, Nick Fenwick. All rights reserved.
  */
 
-define('SOUNDEX_SEARCH_CURRENT_VERSION', '1.0.0');
-define('SOUNDEX_SEARCH_CURRENT_UPDATE_DATE', '2023-11-24');
+define('SOUNDEX_SEARCH_CURRENT_VERSION', '1.0.1');
+define('SOUNDEX_SEARCH_CURRENT_UPDATE_DATE', '2023-11-28');
 define('SOUNDEX_SEARCH_NAME', 'SOUNDEX Search addon');
 
 define('TABLE_SOUNDEX_LOOKUP', DB_PREFIX . 'soundex_lookup');
